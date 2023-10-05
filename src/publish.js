2884494c8f6dc16734f9aa239ca2d1223bc60583
Daniel Montoya <daniel.montoya@eclass.cl>
2023-10-05 08:26:25 -0500

    fix(dps): revert version

--- src/publish.js
+++ src/publish.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict'
 
-const execa = require("execa");
-const getError = require("./get-error");
+const execa = require('execa')
+const getError = require('./get-error')
 
 module.exports = async (_, { cwd, env, stdout, stderr, logger }) => {
   try {
-    logger.log("Deploy app to netlify");
-    const deployResult = execa("netlify", ["deploy", "--prod"], { cwd, env });
+    logger.log('Deploy app to netlify')
+    const deployResult = execa('netlify', ['deploy', '--prod'], { cwd, env })
     deployResult.stdout.pipe(
       stdout,
       { end: false }
-    );
+    )
     deployResult.stderr.pipe(
       stderr,
       { end: false }
-    );
-    await deployResult;
+    )
+    await deployResult
   } catch (err) {
-    throw getError("ENETLIFYDEPLOY");
+    throw getError('ENETLIFYDEPLOY')
   }
-};
+}
--- test/verify.test.js
+++ test/verify.test.js
@@ -1,38 +1,38 @@
-"use strict";
+'use strict'
 
-const { describe, it, before } = require("mocha");
-const { expect } = require("chai");
-const tempy = require("tempy");
-const verify = require("../src/verify");
+const { describe, it, before } = require('mocha')
+const { expect } = require('chai')
+const tempy = require('tempy')
+const verify = require('../src/verify')
 
-describe("Verify", () => {
-  let cwd;
+describe('Verify', () => {
+  let cwd
 
   before(() => {
-    cwd = tempy.directory();
-  });
+    cwd = tempy.directory()
+  })
 
-  it("Return SemanticReleaseError if NETLIFY_AUTH_TOKEN environment variable is not defined", async () => {
+  it('Return SemanticReleaseError if NETLIFY_AUTH_TOKEN environment variable is not defined', async () => {
     try {
-      await verify({}, { cwd });
+      await verify({}, { cwd })
     } catch (err) {
-      expect(err.name).to.equal("SemanticReleaseError");
-      expect(err.code).to.equal("ENONETLIFYTOKEN");
+      expect(err.name).to.equal('SemanticReleaseError')
+      expect(err.code).to.equal('ENONETLIFYTOKEN')
     }
-  });
+  })
 
-  it("Return SemanticReleaseError if NETLIFY_SITE_ID environment variable is not defined", async () => {
+  it('Return SemanticReleaseError if NETLIFY_SITE_ID environment variable is not defined', async () => {
     try {
-      process.env.NETLIFY_AUTH_TOKEN = "my-token";
-      await verify({}, { cwd });
+      process.env.NETLIFY_AUTH_TOKEN = 'my-token'
+      await verify({}, { cwd })
     } catch (err) {
-      expect(err.name).to.equal("SemanticReleaseError");
-      expect(err.code).to.equal("ENONETLIFYSITEID");
+      expect(err.name).to.equal('SemanticReleaseError')
+      expect(err.code).to.equal('ENONETLIFYSITEID')
     }
-  });
+  })
 
-  it("Verify alias from NETLIFY_AUTH_TOKEN environmen variable", async () => {
-    process.env.NETLIFY_SITE_ID = "my-site-id";
-    expect(await verify({}, { cwd })).to.be.a("undefined");
-  });
-});
+  it('Verify alias from NETLIFY_AUTH_TOKEN environmen variable', async () => {
+    process.env.NETLIFY_SITE_ID = 'my-site-id'
+    expect(await verify({}, { cwd })).to.be.a('undefined')
+  })
+})
